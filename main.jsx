fe2fb6a267b46d2e54c2fae8bf8eb46b1dab85e8
Paula Duimovich Fontana <105803147+PaulaDF28@users.noreply.github.com>
2023-09-18 17:23:50 -0300

    Update main.jsx
    react-firabase final

--- main.jsx
+++ main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ContextCart } from "./context/ContextCart.jsx";
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ContextCart>
+      <App />
+    </ContextCart>
   </React.StrictMode>,
 )
